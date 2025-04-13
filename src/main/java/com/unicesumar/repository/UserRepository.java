4b03f8bee462bb9a7d1fd8c85e68f0e999b6266f
Murilo Barbosa <musilvab@gmail.com>
2025-04-13 20:28:53 -0300

    Adição FindByEmail

--- src/main/java/com/unicesumar/repository/UserRepository.java
+++ src/main/java/com/unicesumar/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.unicesumar.repository;
 
-import com.unicesumar.entities.User;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
+import com.unicesumar.entities.User;
 
 public class UserRepository implements EntityRepository<User> {
     private final Connection connection;
@@ -90,4 +90,26 @@ public class UserRepository implements EntityRepository<User> {
             throw new RuntimeException(e);
         }
     }
+
+    public Optional<User> findByEmail(String email) {
+        String query = "SELECT * FROM users WHERE email = ?";
+        try {
+            PreparedStatement stmt = this.connection.prepareStatement(query);
+            stmt.setString(1, email.toString());
+            ResultSet resultSet = stmt.executeQuery();
+            if (resultSet.next()) {
+                return Optional.of(new User(
+                        UUID.fromString(resultSet.getString("uuid")),
+                        resultSet.getString("name"),
+                        resultSet.getString("email"),
+                        resultSet.getString("password")
+                ));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return Optional.empty();
+    }
+    
 }
